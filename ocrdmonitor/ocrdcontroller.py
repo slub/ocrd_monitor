4f8dcad925013d65300a4e6167d6d703a360d92f
Robert Sachunsky <sachunsky@informatik.uni-leipzig.de>
2023-04-04 08:14:12 +0200

    job info: add some logging

--- ocrdmonitor/ocrdcontroller.py
+++ ocrdmonitor/ocrdcontroller.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import sys
+import logging
 from pathlib import Path
 from typing import Protocol
 
@@ -22,23 +23,27 @@ class OcrdController:
     def __init__(self, process_query: ProcessQuery, job_dir: Path) -> None:
         self._process_query = process_query
         self._job_dir = job_dir
+        logging.info(f"process_query: {process_query}")
+        logging.info(f"job_dir: {job_dir}")
 
     def get_jobs(self) -> list[OcrdJob]:
         def is_ocrd_job(j: OcrdJob | None) -> TypeGuard[OcrdJob]:
             return j is not None
 
         job_candidates = [
-            self._try_parse(job_file.read_text())
+            self._try_parse(job_file)
             for job_file in self._job_dir.iterdir()
             if job_file.is_file()
         ]
 
         return list(filter(is_ocrd_job, job_candidates))
 
-    def _try_parse(self, job_str: str) -> OcrdJob | None:
+    def _try_parse(self, job_file: Path) -> OcrdJob | None:
+        job_str = job_file.read_text()
         try:
             return OcrdJob.from_str(job_str)
-        except (ValueError, KeyError):
+        except (ValueError, KeyError) as e:
+            logging.warning(f"found invalid job file: {job_file.name} - {e}")
             return None
 
     def status_for(self, ocrd_job: OcrdJob) -> ProcessStatus | None:

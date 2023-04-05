d4e7e3531bd4d17081191122817fecbc6d9cee3e
Sven Marcus <s.marcus@outlook.de>
2023-04-05 14:45:54 +0200

    Repeat ssh call until it returns something

--- tests/ocrdmonitor/test_sshps.py
+++ tests/ocrdmonitor/test_sshps.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Any, Callable, TypeVar
 
 import pytest
 from testcontainers.general import DockerContainer
@@ -14,6 +15,15 @@ from tests.ocrdmonitor.sshcontainer import (
     openssh_server,
 )
 
+T = TypeVar("T")
+
+
+def run_until_truthy(fn: Callable[..., T], *args: Any) -> T:
+    while not (result := fn(*args)):
+        continue
+
+    return result
+
 
 @pytest.mark.integration
 def test_ps_over_ssh__returns_list_of_process_status(
@@ -21,15 +31,14 @@ def test_ps_over_ssh__returns_list_of_process_status(
 ) -> None:
     process_group = get_process_group_from_container(openssh_server)
 
-    actual = process_status(
-        config=SSHConfig(
-            host="localhost",
-            port=2222,
-            user="testcontainer",
-            keyfile=Path(KEYDIR) / "id.rsa",
-        ),
-        process_group=process_group,
+    config = SSHConfig(
+        host="localhost",
+        port=2222,
+        user="testcontainer",
+        keyfile=Path(KEYDIR) / "id.rsa",
     )
+
+    actual = run_until_truthy(process_status, config, process_group)
 
     first_process = actual[0]
     assert first_process.pid == process_group

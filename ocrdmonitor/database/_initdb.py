6aa918bf1157b6e4a820b08ec4343c8d4f3ea231
ocrd <ocrd@sdvocrdmanager.slub-dresden.de>
2024-03-06 22:06:24 +0100

    try to make motor.motor_asyncio.AsyncIOMotorClient work with strict mypy

--- ocrdmonitor/database/_initdb.py
+++ ocrdmonitor/database/_initdb.py
@@ -39,8 +39,8 @@ def __beanie_initializer() -> InitDatabase:
 
         __initialized = True
         connection_str = rebuild_connection_string(connection_str)
-        client = AsyncIOMotorClient(connection_str)
-        client.get_io_loop = asyncio.get_event_loop
+        client = AsyncIOMotorClient(connection_str) # type: ignore[var-annotated]
+        client.get_io_loop = asyncio.get_event_loop # type: ignore[method-assign]
         await init_beanie(
             database=client.ocrd,
             document_models=[BrowserProcess, MongoOcrdJob],

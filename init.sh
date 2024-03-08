690fe6467241db72b694b71e467cb8e7bbc9d5fe
ocrd <ocrd@sdvocrdmanager.slub-dresden.de>
2024-03-08 01:41:16 +0100

    workspace prefix must be /data instead of /data/ocr-d

--- init.sh
+++ init.sh
@@ -2,7 +2,15 @@
 
 export MONITOR_DB_CONNECTION_STRING=$MONITOR_DB_CONNECTION
 export OCRD_BROWSER__MODE=native
-export OCRD_BROWSER__WORKSPACE_DIR=/data/ocr-d
+# all OCR-D workspaces on the Manager are under /data/ocr-d
+# but since the Manager resolves everything under /data
+# it tracks the workspace directory relative to that in the database
+# (e.g. ocr-d/testdata-production)
+# so if we write /data/ocr-d, we could list workspaces fine,
+# but our workspace URLs from the job database would be wrong
+# (resolving as /data/ocr-d/ocr-d/...)
+# so better just use /data as well here:
+export OCRD_BROWSER__WORKSPACE_DIR=/data
 export OCRD_BROWSER__PORT_RANGE="[9000,9100]"
 export OCRD_LOGVIEW__PORT=$MONITOR_PORT_LOG
 export OCRD_MANAGER__URL=$MANAGER_URL
